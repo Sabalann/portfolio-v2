2f4b2cbcaff2b2455ae177f76020c3ebe8ed69c8
Sabalan Alaeiyeh <s.alaeiyeh@students.uu.nl>
2025-01-17 20:51:13 +0100

    nav didn't work

---
+++ app/main/WhoAmI.jsx
@@ -0,0 +1,8 @@
+export default function WhoAmI() {
+    return (
+        <>
+            <h1 className="title">Hoi, ik ben Sabalan! <br/> ik ontwerp en ontwikkel websites met een oog op het gevoel die de websites afgeven</h1>
+        </>
+    );
+
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import WhoAmI from "./main/WhoAmI";
+import "./main/main.css"
+import Nav from "./main/Nav";
 
 export default function Home() {
   return (
     <>
-      <h1>Sabalan Alaeiyeh</h1>
+      <section id="whoAmI">
+        <WhoAmI/>
+      </section>
     </>
   );
 }

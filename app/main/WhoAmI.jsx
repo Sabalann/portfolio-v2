c152d1e5ebe95922b0d207a33a21601a26321918
Sabalan Alaeiyeh <s.alaeiyeh@gmail.com>
2025-02-05 16:19:09 +0100

    users can select who they are and are displayed a message tailored to them

--- app/main/WhoAmI.jsx
+++ app/main/WhoAmI.jsx
@@ -1,11 +1,49 @@
+'use client'
+import { useState } from "react";
+
+
 export default function WhoAmI() {
+    const [selectedAudience, setSelectedAudience] = useState("everyone");
+
+    const textOptions = {
+        everyone: "Welkom! Ik ben Sabalan en ik hou ervan om digitale ervaringen te creëren. Mijn focus ligt voornamelijk op het ontwikkelen van websites, maar ik ben ook actief in het maken van games, het bewerken van video’s en foto’s en nog veel meer. Kortom, ik ben een veelzijdige maker met een brede creatieve interesse.",
+        recruiters: "Ik ben een front-end developer met 2 jaar ervaring. Ik heb technologiën als Nextjs, Reactjs en Tailwind onder de knie. Bovendien ben ik bezig met het behalen van mijn Computer Science bachelor diploma.",
+        clients: "Heb je een website nodig die een bepaald gevoel moet kunnen oproepen? Eentje die mooi, uniek, snel en passend bij jou of jouw bedrijf is? Goed nieuws! Je hoeft niet verder te zoeken."
+    };
+
     return (
         <>
             <div className="intro">
-                <h1 className="title">Hoi, ik ben Sabalan en ik ben een web-developer!</h1>
-                <h2 className=" text-neutral-200 text-2xl mt-4">
-                    Dat houdt in dat ik websites design en ontwikkel...
-                </h2>
+                <div className="flex gap-4">
+                    {["everyone", "recruiters", "clients"].map((option) => (
+                    <label key={option} className="flex items-center gap-2 cursor-pointer">
+                        <input
+                        type="radio"
+                        name="audience"
+                        value={option}
+                        checked={selectedAudience === option}
+                        onChange={(e) => setSelectedAudience(e.target.value)}
+                        className="hidden"
+                        />
+                        <span
+                        className={`py-2 text-base ${
+                            selectedAudience === option ? " text-white" : "text-gray-400"
+                        }`}
+                        >
+                        {option === "everyone"
+                            ? "Voor iedereen"
+                            : option === "recruiters"
+                            ? "Recruiters"
+                            : "Website-nodig-hebbers"}
+                        </span>
+                    </label>
+                    ))}
+                </div>
+
+                <h1 className="text-white text-4xl">
+                    {textOptions[selectedAudience]}
+
+                </h1>
             </div>
         </>
     );

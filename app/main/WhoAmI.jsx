5178adec4e96ee0b737bb9319b1156e59795a105
Sabalan Alaeiyeh <s.alaeiyeh@gmail.com>
2025-02-10 22:31:34 +0100

    glowing animation for user roles to make it obvious they're clickable

--- app/main/WhoAmI.jsx
+++ app/main/WhoAmI.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 export default function WhoAmI() {
     const [selectedAudience, setSelectedAudience] = useState("everyone");
-    const arrowSrc = "/arrow.jpeg"
+    const arrowSrc = "/arrow.png"
 
     const textOptions = {
         everyone: "Welkom! Ik ben Sabalan en ik hou ervan om digitale ervaringen te creëren. Mijn focus ligt voornamelijk op het ontwikkelen van websites, maar ik ben ook actief in het maken van games, het bewerken van video’s en foto’s en nog veel meer. Kortom, ik ben een veelzijdige maker met een brede creatieve interesse.",
@@ -17,7 +17,7 @@ export default function WhoAmI() {
         <>
             <div className="intro">
                 <div className="flex gap-4">
-                    <Image className="arrow-img" src={arrowSrc} width={50} height={50} alt="arrow"></Image>
+                    {/* <Image className="arrow-img" src={arrowSrc} width={50} height={50} alt="arrow"></Image> */}
                     {["everyone", "recruiters", "clients"].map((option) => (
                     <label key={option} className="flex items-center gap-2 cursor-pointer">
                         <input
@@ -29,8 +29,8 @@ export default function WhoAmI() {
                         className="hidden"
                         />
                         <span
-                        className={`py-2 text-base transition-all duration-500 ease-in-out ${
-                            selectedAudience === option ? " text-white" : "text-gray-400"
+                        className={`options py-2 text-base transition-all duration-500 ease-in-out ${
+                            selectedAudience === option ? " text-white" : "options text-gray-400"
                         }`}
                         >
                         {option === "everyone"
